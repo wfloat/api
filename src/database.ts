87a6c09361772ce898f73c16bac93adf1823fbbe
Mitchell Sayre <sayremitch@gmail.com>
2024-01-22 17:00:44 -0500

    Fix docker compose database connection string

--- src/database.ts
+++ src/database.ts
@@ -2,13 +2,19 @@ import { DB } from "../prisma/generated/kysely.js";
 import pg from "pg";
 import { Kysely, PostgresDialect } from "kysely";
 
+const POSTGRES_USER = process.env.POSTGRES_USER;
+const POSTGRES_DB = process.env.POSTGRES_DB;
+const POSTGRES_PW = process.env.POSTGRES_PW;
+const POSTGRES_PORT = parseInt(process.env.POSTGRES_PORT!);
+const POSTGRES_SERVICE_NAME = process.env.POSTGRES_SERVICE_NAME;
+
 const dialect = new PostgresDialect({
   pool: new pg.Pool({
-    database: "wfloat-local",
-    host: "localhost",
-    user: "postgres",
-    password: "password",
-    port: 5432,
+    database: POSTGRES_DB,
+    host: POSTGRES_SERVICE_NAME,
+    user: POSTGRES_USER,
+    password: POSTGRES_PW,
+    port: POSTGRES_PORT,
     max: 10,
   }),
 });
