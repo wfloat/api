97d605f405e03e7b8949e2b54b86f9ca26508e78
Mitchell Sayre <sayremitch@gmail.com>
2024-01-27 23:00:55 -0500

    Add paging connection for AIHubVoiceModel BackupUrls

--- src/schema/ai-hub-voice-model/ai-hub-voice-model.ts
+++ src/schema/ai-hub-voice-model/ai-hub-voice-model.ts
@@ -28,7 +28,68 @@ builder.prismaObject("AIHubVoiceModel", {
       {
         type: "VoiceModelBackupUrl",
         cursor: "id",
-        resolve: async (query, parent, args, context, info) => undefined,
+        resolve: async (query, parent, args, context, info) => {
+          let requestedLimit = args.first ?? args.last ?? 0;
+          let limit = Math.min(requestedLimit, PAGE_LIMIT);
+          limit = limit + 1; // Retrieve an extra record used for pageInfo hasNextPage/hasPreviousPage
+
+          let cursor = query.cursor?.id;
+          let cursorRow: VoiceModelBackupUrl | null = null;
+          if (cursor) {
+            cursorRow = await context.loaders.voiceModelBackupUrl.load(cursor);
+            // TODO: Error handle if cursorRow not found
+          }
+
+          // Based on https://the-guild.dev/blog/graphql-cursor-pagination-with-postgresql
+          let result = await db
+            .selectFrom("VoiceModelBackupUrl")
+            .select(["id", "url"])
+
+            .$if(args.first ? true : false, (qb) => qb.orderBy(["url asc", "id asc"]))
+            .$if(args.last ? true : false, (qb) => qb.orderBy(["url desc", "id desc"]))
+
+            // .$if(args.minDownloadCount ? true : false, (qb) =>
+            //   qb.where("downloadCount", ">=", args.minDownloadCount!)
+            // )
+
+            .where("voiceModelId", "=", parent.id)
+
+            .$if(args.after && cursorRow ? true : false, (qb) =>
+              qb.where((eb) =>
+                eb.or([
+                  eb("url", "=", cursorRow?.url!).and("id", ">", cursorRow?.id!),
+                  eb("url", ">", cursorRow?.url!),
+                ])
+              )
+            )
+            .$if(args.before && cursorRow ? true : false, (qb) =>
+              qb.where((eb) =>
+                eb.or([
+                  eb("url", "=", cursorRow?.url!).and("id", "<", cursorRow?.id!),
+                  eb("url", "<", cursorRow?.url!),
+                ])
+              )
+            )
+
+            .limit(limit)
+            .execute();
+
+          let ids = result.map((row) => row.id);
+          let rows = await Promise.all(
+            ids.map((id) => context.loaders.voiceModelBackupUrl.load(id))
+          );
+
+          if (rows.some((item) => item === null)) {
+            throw new Error(
+              "One or more records returned by the loader are either null or instanceof Error."
+            );
+          } else {
+            if (args.last) {
+              rows = rows.reverse();
+            }
+            return rows as VoiceModelBackupUrl[];
+          }
+        },
       },
       { name: "AIHubVoiceModelBackupUrlsConnection" },
       { name: "AIHubVoiceModelBackupUrlsEdge" }
